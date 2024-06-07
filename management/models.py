0a822341effd06e34a7d339d24fad032fe5cad6a
10 Minute Pogram <10minuteprogram@gmail.com>
2024-06-07 10:03:03 +0600

    added model file

--- management/models.py
+++ management/models.py
@@ -6,4 +6,9 @@ class Contact(models.Model):
     phone = models.CharField(max_length=20)
     age = models.IntegerField()
     address = models.TextField()
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+
+    def __str__(self):
+        return f"{self.name} | {self.age}"
