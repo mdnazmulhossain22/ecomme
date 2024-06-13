44cc437eca281bbb3b5e4c5100e751a075fb6d4a
10 Minute Pogram <10minuteprogram@gmail.com>
2024-06-13 10:21:01 +0600

    add contact function

--- management/urls.py
+++ management/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('', home, name="home"),
     path('contacts/', contact, name="contact"),
+    path('contacts/detail/<int:id>', detail, name="detail"),
     path('about/', about, name="about"),
 ]
